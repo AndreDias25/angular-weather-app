cf0979379d33af6d8a696667840bc96e7b3ea2d8
André Dias <andre25dias@gmail.com>
2024-05-31 20:16:31 -0300

    :rocket: Atualizando paths do projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiKey:  process.env["API_KEY"] || 'yMAsM2KSxl7c4CXFnj9vbVgbI3CYMq3Y',
-  apiAutoCompleteSearch: process.env["API_AUTOCOMPLETE"] || 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-  apiCurrentCondition: process.env["API_CURRENTCONDITION"] || 'http://dataservice.accuweather.com/currentconditions/v1/',
-  apiNext12Hours: process.env["API_NEXT12HOURS"] || 'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/'
+  apiKey:  'yMAsM2KSxl7c4CXFnj9vbVgbI3CYMq3Y',
+  apiAutoCompleteSearch: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+  apiCurrentCondition: 'http://dataservice.accuweather.com/currentconditions/v1/',
+  apiNext12Hours: 'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/'
 };

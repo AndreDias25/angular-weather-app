f05270ddede42006b84b9a314d03bfad4016b17a
André Dias <andre25dias@gmail.com>
2024-06-02 18:16:34 -0300

    :rocket: Novo teste para o deploy

--- src/app/services/accuweather-api.service.ts
+++ src/app/services/accuweather-api.service.ts
@@ -1,33 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
 import { CityAutoComplete } from '../models/CityAutoComplete';
 import { CityCurrentCondition } from '../models/CityCurrentCondition';
 import { Next12Hours} from '../models/Next12Hours';
 import { RandomQuote } from '../models/RandomQuote';
+import { handler } from 'functions/fetch-api-key/fetch-api-key';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccuweatherApiService {
-  private cityautoComplete:string = environment.apiAutoCompleteSearch;
-  private cityCurrentCondition:string = environment.apiCurrentCondition;
-  private Next12hours:string = environment.apiNext12Hours;
-  private apiKey:string = environment.apiKey;
+  private cityautoComplete:string = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete";
+  private cityCurrentCondition:string = "http://dataservice.accuweather.com/currentconditions/v1/";
+  private Next12hours:string = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
+  private apiKey:BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor(private http: HttpClient) { }
 
+  fetchApiKey(): void {
+    this.http.get<{ apiKey: string }>('https://weatherangularapp1.netlify.app/.netlify/functions/fetch-api-key').pipe(
+      tap(response => this.apiKey.next(response.apiKey))
+    ).subscribe();
+  }
+
   getCityAutoComplete(cityName:string):Observable<CityAutoComplete[]>{
-    return this.http.get<CityAutoComplete[]>(`${this.cityautoComplete}?apikey=${this.apiKey}&q=${cityName}`)
+    return this.apiKey.pipe(
+      switchMap(apiKey => this.http.get<CityAutoComplete[]>(`${this.cityautoComplete}?apikey=${apiKey}&q=${cityName}`))
+    );
   }
 
   getCurrentCondition(cityKey:string | null | undefined){
-    return this.http.get<CityCurrentCondition[]>(`${this.cityCurrentCondition}${cityKey}?apikey=${this.apiKey}&details=true`)
+    return this.apiKey.pipe(
+      switchMap(apiKey => this.http.get<CityCurrentCondition[]>(`${this.cityCurrentCondition}${cityKey}?apikey=${apiKey}&details=true`))
+    );
   }
 
   getNext12hours(cityKey:string){
-    return this.http.get<Next12Hours[]>(`${this.Next12hours}${cityKey}?apikey=${this.apiKey}&metric=true`)
+    return this.apiKey.pipe(
+      switchMap(apiKey => this.http.get<Next12Hours[]>(`${this.Next12hours}${cityKey}?apikey=${apiKey}&metric=true`))
+    );
   }
 
   getRandomQuote(){
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey:  'yMAsM2KSxl7c4CXFnj9vbVgbI3CYMq3Y',
-  apiAutoCompleteSearch: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-  apiCurrentCondition: 'http://dataservice.accuweather.com/currentconditions/v1/',
-  apiNext12Hours: 'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/'
-};

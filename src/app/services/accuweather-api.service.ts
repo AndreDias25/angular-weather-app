9605af06cb13691ec17170892cb9d6233339c787
André Dias <andre25dias@gmail.com>
2024-06-06 17:15:04 -0300

    :broom: Limpeza e refatoração dos paths da api

--- src/app/services/accuweather-api.service.ts
+++ src/app/services/accuweather-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map, switchMap, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { CityAutoComplete } from '../models/CityAutoComplete';
 import { CityCurrentCondition } from '../models/CityCurrentCondition';
 import { Next12Hours} from '../models/Next12Hours';
@@ -14,36 +14,18 @@ interface ApiKeyResponse {
   providedIn: 'root'
 })
 export class AccuweatherApiService {
-  private cityautoComplete:string = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete";
-  private cityCurrentCondition:string = "http://dataservice.accuweather.com/currentconditions/v1/";
-  private Next12hours:string = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
-  private apiKey!: string;
-
-  constructor(private http: HttpClient) {
-    this.fetchApiKey();
-   }
-
-  private fetchApiKey(): void {
-    this.http.get<ApiKeyResponse>("https://weather-app-backend-one-opal.vercel.app/api/get-key").subscribe(
-      (response) => {
-        this.apiKey = response.apiKey;
-      },
-      (error) => {
-        console.error('Erro:', error);
-      }
-    )
-  }
+  constructor(private http: HttpClient) {}
 
   getCityAutoComplete(cityName:string):Observable<CityAutoComplete[]>{
-    return this.http.get<CityAutoComplete[]>(`${this.cityautoComplete}?apikey=${this.apiKey}&q=${cityName}`)
+    return this.http.get<CityAutoComplete[]>(`https://weather-app-backend-one-opal.vercel.app/api/cities/autocomplete/:${cityName}`)
   }
 
   getCurrentCondition(cityKey:string | null | undefined){
-    return this.http.get<CityCurrentCondition[]>(`${this.cityCurrentCondition}${cityKey}?apikey=${this.apiKey}&details=true`)
+    return this.http.get<CityCurrentCondition[]>(`https://weather-app-backend-one-opal.vercel.app/api/currentconditions/:${cityKey}`)
   }
 
   getNext12hours(cityKey:string){
-    return this.http.get<Next12Hours[]>(`${this.Next12hours}${cityKey}?apikey=${this.apiKey}&metric=true`)
+    return this.http.get<Next12Hours[]>(`https://weather-app-backend-one-opal.vercel.app/api/forecasts/12hour/:${cityKey}`)
   }
 
   getRandomQuote(){
